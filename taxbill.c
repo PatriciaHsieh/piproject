19e0bfc264c0973ff23ccb5101d9082256009896
jessicaj20 <50212072+jessicaj20@users.noreply.github.com>
2019-06-06 10:22:55 -0700

    Update and rename taxbill to taxbill.c

--- taxbill.c
+++ taxbill.c
@@ -1,25 +1,20 @@
 #include <stdio.h>
 
 //declare taxbill function, s=subtotal, C, T, N=salestax, t=taxedbill
-float addtax (float s, float C, float T, float N)
+void addtax (float s, float* C, float* T, float* N)
 {
-  C = s * 1.075;
-  T = s * 1.065;
-  N = s * 1.08875;
-  return C;
-  return T;
-  return N;
+  *C = s * 1.075;
+  *T = s * 1.065;
+  *N = s * 1.08875;
 }
 
 int main(void) 
 {
-   float s = 123;
+   float s = 411;
    float C;
    float N;
    float T;
-   C = addtax(s, C, T, N);
-   T = addtax(s, C, T, N);
-   N = addtax(s, C, T, N);
+   addtax(s, &C, &T, &N)
    printf("California taxed bill is $%f\n", C);
    printf("New York City taxed bill is $%f\n", N);
   printf("Texas taxed bill is $%f\n", T);
