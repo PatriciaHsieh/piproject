1839afa27f10510b1d2225b7ebad726c1c4d38f5
PatriciaHsieh <50212235+PatriciaHsieh@users.noreply.github.com>
2019-06-06 09:52:01 -0700

    made splitbill.c code (without input)

--- splitbill.c
+++ splitbill.c
@@ -1,7 +1,19 @@
-#include<stdio.h>
+#include <stdio.h>
 
-//declare split bill function
+//declare splitbill function, s=subtotal, n=number of people, t=total
+float splitbill (float s, float n, float t)
+{
+  t = s / n;
+  return t;
+}
 
-int main()
+int main(void) 
+{
+  //call splitbill
+ float t;
+ float s = 100;
+ float n = 5;
+ t = splitbill(s, n, t);
+ printf("Each person will pay $%f\n", t);
 
-//call split bill function
+}
