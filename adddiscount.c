72962661b5ac2c53ef440dd92c508da867941b7f
PatriciaHsieh <50212235+PatriciaHsieh@users.noreply.github.com>
2019-06-07 11:01:08 -0700

    edited adddiscount code

--- adddiscount.c
+++ adddiscount.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
 
-//declare addtip function, s=subtotal, r=rate, t=tip
-float adddiscount (float s, float r, float t)
+//declare adddiscount function, s=subtotal, d=discount, newsub=new subtotal
+float adddiscount (float s, float d, float newsub)
 {
-  t = s * (100-r) / 100;
-  return t;
+  newsub = s * (100-d) / 100;
+  return newsub;
 }
 
 int main(void) 
 {
-  //call adddiscount
-  for (float i=0; i<31; i+=5)
-  {
-    float r = i;
-    float t;
-    float s = 100;
-    t = adddiscount(s, r, t);
-    printf("If you have a %f percent discount, the new subtotal is $%f\n", r, t);
-  }
+//call adddiscount
+for (float i=0; i<31; i+=5)
+ {
+   float d = i;
+   float newsub;
+   float s = 100;
+   newsub = adddiscount(s, d, newsub);
+   printf("If you have a %f percent discount, the new subtotal is $%f\n", d, newsub);
+   }
 }
